5e9f5163de7f48886f3d0e49070256b88e551eb7
Safae-e <Safae.el.jaouhari@hva.nl>
2025-01-17 09:39:50 +0100

    interactie filtermenu gemaakt (#4)

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+// let mijnButton = document.querySelector('button')
+// let mijnDiv = document.querySelector('div')
 
-  
+// mijnButton.addEventListener('click', function() {
+//   mijnDiv.classList.toggle('show-menu')
+// })
+
+function myFunction() {
+    var filter = document.getElementById("filter");
+    if (filter.style.display === "none") {
+      filter.style.display = "block";
+    } else {
+      filter.style.display = "none";
+    }
+  }
